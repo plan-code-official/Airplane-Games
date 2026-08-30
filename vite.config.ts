a902b6f739da5cce18c53a2c3b40e0258d206365
fady nasser <fadynn7@gmail.com>
2026-08-30 13:17:33 +0300

    style: increase cloud bubble options font size by 1.5x

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true, // Optional: Set to true to expose the server on your local network
+    strictPort: true, // Optional: Exits if the port is already in use instead of trying the next available one
+  },
 })
